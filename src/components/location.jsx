de991627f6b2d815278a96bcece7cab1e5af73dd
hololee <lccandol@naver.com>
2025-02-02 01:36:09 +0900

    fix: 지도 위치 수정

--- src/components/location.jsx
+++ src/components/location.jsx
@@ -47,8 +47,8 @@ const Location = () => {
   const executeScript = () => {
     const scriptTag = document.createElement("script");
     const inlineScript = document.createTextNode(`new daum.roughmap.Lander({
-    "timestamp" : "1652464367301",
-    "key" : "2a8fe",
+    "timestamp" : "1738426071785",
+    "key" : "2mwhu",
     "mapWidth" : "640",
     "mapHeight" : "360"
   }).render();`);
@@ -61,7 +61,7 @@ const Location = () => {
   const InstallScript = () => {
     (function () {
       let c = window.location.protocol === "https:" ? "https:" : "http:";
-      let a = "16137cec";
+      let a = "07708ab0";
 
       if (window.daum && window.daum.roughmap && window.daum.roughmap.cdn) {
         return;
@@ -99,27 +99,37 @@ const Location = () => {
       </Divider>
       <Image src={Flower} />
       <Map
-        id="daumRoughmapContainer1652464367301"
+        id="daumRoughmapContainer1738426071785"
         className="root_daum_roughmap root_daum_roughmap_landing"
       ></Map>
       <Content>
-        대구 수성구 두산동 888-2번지
+        서울 구로구 새말로 97
         <br />
-        호텔수성 수성스퀘어 3층 피오니홀
+        신도림테크노마트 웨딩시티 11층 그랜드 볼룸
         <br />
         <br />
         <Title>버스 이용시</Title>
         <br />
         <br />
-        410-1, 401 호텔수성 앞 하차
+        신도림역 (17-102)정류장 하차
         <br />
-        수성1-1, 수성3-1, 814 TBC방송국 앞 하차
+        신도림역 (17-001)정류장 하차
         <br />
         <br />
         <Title>지하철 이용시</Title>
         <br />
         <br />
-        3호선 수성못역 하차 (도보 10분)
+        1호선 2호선 신도림역
+        <br />
+        (신도림역 3번출구 테크노마트 판매동 지하1층과 직접 연결되어 있습니다.)
+        <br />
+        <br />
+        <Title>주차안내</Title>
+        <br />
+        <br />
+        테크노마트 지하주차장 이용(B3~B7)
+        <br />
+        (주차요원의 안내를 받으세요.)
       </Content>
     </Wrapper>
   );
